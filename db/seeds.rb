4d667cf0c97a644972ef7f910b569f82e98e07be
olivyeshka <olyahryhorash404@gmail.com>
2021-11-26 13:37:37 +0200

    Lab11

---
+++ db/seeds.rb
@@ -0,0 +1,39 @@
+Product.delete_all
+# . . .
+Product.create!(title: 'HARRY POTTER WITH HOGWARTS LETTERS',
+description:
+%{<p>
+    Hogwarts School of Witchcraft and Wizardry has been trying to reach Mr. Harry Potter for weeks and finally the letters come flooding in to Mr. Dursley's dismay. 
+    Commemorate the moment in your collection with the Funko exclusive Pop! Deluxe Harry Potter with Hogwarts Letters. 
+    Vinyl figure is approximately 4.5-inches tall.
+</p>},
+image_url: 'harry_potter.jpg',
+price: 30.00)
+Product.create!(title: 'SPIDER-MAN UPGRADED SUIT - SPIDER-MAN: NO WAY HOME',
+description:
+%{<p>
+    Your Spider-Man: No Way Home collection may not see Peter Parker swing into its ranks in his new upgraded suit. 
+    This Pop! Spider-Man Upgraded Suit figure will make a great addition to any Marvel set. 
+    Vinyl bobblehead is approximately 4-inches tall.
+</p>},
+image_url: 'spider_man_no_way_home.jpg',
+price: 12.00)
+Product.create!(title: 'PIKACHU SITTING - POKÉMON',
+description:
+%{<p>
+    Your dream of becoming a Trainer is not out of reach! 
+    Catch this smiling Pop! Pikachu to add to your Pokémon collection. 
+    Vinyl figure is approximately 3.75-inches tall.
+</p>},
+image_url: 'pikachu.jpg',
+price: 12.00)
+Product.create!(title: 'MALEFICENT - MALEFICENT: MISTRESS OF EVIL',
+description:
+%{<p>
+    A wedding is in the works but Maleficent has made her opinion clear. 
+    How will things turn out in your collection of Maleficent: Mistress of Evil when Pop! Maleficent arrives? 
+    Vinyl figure is approximately 4.75-inches tall.
+</p>},
+image_url: 'maleficent.jpg',
+price: 12.00)    
+# . . .
